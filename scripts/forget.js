2aaf903f7ae9dbec21c95f78f75a3e58c3d30745
sam <samueladeleke302@gmail.com>
2023-05-15 17:41:54 +0100

    first commit

---
+++ scripts/forget.js
@@ -0,0 +1,4 @@
+function resetPassword() {
+    var code = document.getElementById('number').value;
+    alert("A password reset link has been sent to " + code);
+}
---
+++ scripts/main.js
@@ -0,0 +1,4 @@
+function welcome() {
+    var x1 = document.getElementById('user').value;
+    alert("Welcome back " + x1);
+        }
